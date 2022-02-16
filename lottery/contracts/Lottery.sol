72bacf979eb7eedc55e0147322de240109563d76
theo <theophilus96@gmail.com>
2022-02-16 22:46:40 +0800

    Lottery

---
+++ lottery/contracts/Lottery.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract Lottery{
+    address public manager;
+    address[] public players;
+
+    constructor(){
+        manager= msg.sender;
+    }
+
+    function enter() public payable{
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
+    }
+
+    function random() private view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+
+    function pickWinner() public restrictedManager{
+        uint index= random()%players.length;
+        payable(players[index]).transfer(address(this).balance);
+        players = new address[](0);
+    }
+
+    modifier restrictedManager(){
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns(address[] memory){
+        return players;
+    }
+}
